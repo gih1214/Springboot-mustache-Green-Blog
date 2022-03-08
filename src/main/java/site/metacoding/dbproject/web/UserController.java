fad4d54b65f8b10635359e9ebafe2bdcdc901160
gih1214 <gih1214@hanmail.net>
2022-03-08 09:18:43 +0900

    6. 로그아웃 처리 완료

--- src/main/java/site/metacoding/dbproject/web/UserController.java
+++ src/main/java/site/metacoding/dbproject/web/UserController.java
@@ -131,6 +131,7 @@ public class UserController {
     // 로그아웃 - 로그인O
     @GetMapping("/logout")
     public String logout() {
-        return "메인페이지를 돌려주면 됨"; // PostController 만들고 수정하자.
+        session.invalidate(); // 영역 전체를 날리는 것
+        return "redirect:/loginForm"; // PostController 만들고 수정하자.
     }
 }
