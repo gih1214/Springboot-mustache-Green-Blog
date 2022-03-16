df9329db90e5343c11a6baa3abce39dd8c193ef7
gih1214 <gih1214@hanmail.net>
2022-03-16 18:03:49 +0900

    19. 회원정보 수정 - 더티체킹 사용

--- src/main/java/site/metacoding/dbproject/service/UserService.java
+++ src/main/java/site/metacoding/dbproject/service/UserService.java
@@ -46,8 +46,19 @@ public class UserService {
     }
 
     @Transactional
-    public void 유저수정() {
+    public User 유저수정(Integer id, User user) {
+        // 1. 영속화
+        Optional<User> userOp = userRepository.findById(id);
 
-    }
+        if (userOp.isPresent()) { // 영속화 됨
+            User userEntity = userOp.get();
+            userEntity.setPassword(user.getPassword());
+            userEntity.setEmail(user.getEmail());
+
+            return userEntity;
+        }
+
+        return null;
+    } // 트랜잭션 종료 + 영속화 되어 있는 것들 전부 더티체킹(변경감지해서 디비에 flush)함.
 
 }
